7afe960555d9550a71eddf9713a0518609f19745
codervivek496 <viveksinghssm496@gmail.com>
2022-04-20 10:00:18 +0530

    uc2

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,24 @@
 #flip Coin Simulator
+declare -A simulator
+h=0
+t=0
+for ((i=0; i<1000; i++))
+do
 flip=$(( RANDOM%2 ))
 if [ $flip -eq 0 ]
 then
-	echo "Head"
+	h=$((h+1))
 else
-	echo "Tail"
+	t=$((t+1))
 fi
+done
+simulator=(["Heads"]=$h ["Tails"]=$t)
+for coin in "${!simulator[@]}";
+do
+	echo $coin - ${simulator[$coin]};
+done
+headPercent=`expr $h / 10`
+tailPercent=`expr 100 - $headPercent`
+echo "Head percent: $headPercent"
+echo "Tail percent: $tailPercent"
+
